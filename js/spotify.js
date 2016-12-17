e52a6d7d2c134335c315e1533c33c363d9a11dbb
Ryan Dejaegher <dejaegher.ryan@gmail.com>
2016-12-17 16:00:57 -0600

    Added track/album info to lightbox caption using "data-title" attribute
    Added track/album info to lightbox caption using "data-title" attribute
    
    Pulling data from Spotify API:
    
    - Artist
    - Track Title
    - Album
    - Popularity
    - Explicit lyrics
    
    Also added dynamic H1 tag that will update artist name based on artist
    ID submitted to API

--- js/spotify.js
+++ js/spotify.js
@@ -2,25 +2,36 @@
 
 $(document).ready(function () {
 
-///Gets album art at 600px and 300px for the thumbnail and the lightbox
+///Gets album art at 640px and 300px for the thumbnail and the lightbox
   var spotify = "https://api.spotify.com/v1/artists/5K4W6rqBFWDnAN6FQUkS6x/top-tracks?country=CA";
+  
   function displayAlbums(data){
     var html = '<ul>';
-    $.each(data.tracks, function(i,eachTrack){
+	var artistHeader ='';  
+    $.each(data.tracks, function(i, eachTrack){
+	  console.log(eachTrack.name);	
       html += '<li>';
-	  html += '<a href="' + eachTrack.album.images[0].url + '"data-lightbox="albums">';	
+	  html += '<a href="' + eachTrack.album.images[0].url + '"data-lightbox="albums" '+' data-title="<p>Title:<span>' + eachTrack.name + '</span></p><p>Artist:<span>' + eachTrack.album.artists[0].name + '</span></p> <p>Album:<span>' + eachTrack.album.name + '</span</p><p>Popularity: <span> ' + eachTrack.popularity + ' </span></p><p>Explicit: <span>' + eachTrack.explicit + ' </span></p>">';	
       html += '<img src="' + eachTrack.album.images[1].url + '"data-lightbox="albums">';
 	  html += '</a>';	
       html += '</li>';
-	  /*html += '<h2>"' + eachTrack.name + '"</h2>';*/
+	  artistHeader = 'Top Tracks From ' + eachTrack.album.artists[0].name + ' in Canada';	
+	  console.log(eachTrack.name);
     });
     html += '</ul>';
     $(".spotify").html(html);
+	$("h1").html(artistHeader); 
 
   }
 	
   $.getJSON(spotify,displayAlbums);
 /*});*/
+	
+	$(".spotify a").click(function (event) {
+		event.preventDefault();
+		var href = $(this).attr("href");
+		console.log(spotify);
+	});
 	
 //// Lightbox Options	
 	lightbox.option ({
